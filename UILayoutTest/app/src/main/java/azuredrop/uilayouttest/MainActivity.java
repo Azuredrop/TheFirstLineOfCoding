8c1f75ea4a16e684825d652d30168f7e04119358
AzureDrop <taozhanbiao@gmail.com>
2017-04-14 10:49:08 +0800

    20170414_01 Chapter3  --  UI Layout

--- UILayoutTest/app/src/main/java/azuredrop/uilayouttest/MainActivity.java
+++ UILayoutTest/app/src/main/java/azuredrop/uilayouttest/MainActivity.java
@@ -1,13 +1,43 @@
 package azuredrop.uilayouttest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button1 = (Button)findViewById(R.id.button1);
+        button1.setOnClickListener(this);
+        Button button2 = (Button)findViewById(R.id.button2);
+        button2.setOnClickListener(this);
+        Button button3 = (Button)findViewById(R.id.button3);
+        button3.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = null;
+        switch (v.getId()){
+            case R.id.button1:
+                intent = new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(intent);
+                break;
+            case R.id.button2:
+                intent = new Intent(MainActivity.this, Main3Activity.class);
+                startActivity(intent);
+                break;
+            case R.id.button3:
+                intent = new Intent(MainActivity.this, Main4Activity.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
     }
 }
