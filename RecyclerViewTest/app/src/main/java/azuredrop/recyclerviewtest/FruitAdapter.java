b7d806d5ee8302f84001668f291ffa80314ff81f
AzureDrop <taozhanbiao@gmail.com>
2017-04-15 10:10:20 +0800

    20170415_01 Chapter3  --  RecyclerView

--- RecyclerViewTest/app/src/main/java/azuredrop/recyclerviewtest/FruitAdapter.java
+++ RecyclerViewTest/app/src/main/java/azuredrop/recyclerviewtest/FruitAdapter.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -15,26 +16,46 @@ import java.util.List;
 
 public class FruitAdapter extends RecyclerView.Adapter<FruitAdapter.ViewHolder> {
     private List<Fruit> mFruitList;
+    private int mItemLayout;
 
     static class ViewHolder extends RecyclerView.ViewHolder{
+        View fruitView;
         ImageView fruitImage;
         TextView fruitName;
 
         public ViewHolder(View view){
             super(view);
+            fruitView = view;
             fruitImage = (ImageView)view.findViewById(R.id.fruit_image);
             fruitName = (TextView)view.findViewById(R.id.fruit_name);
         }
     }
 
-    public FruitAdapter(List<Fruit> fruitList){
+    public FruitAdapter(List<Fruit> fruitList, int itemlayout){
         mFruitList = fruitList;
+        mItemLayout = itemlayout;
     }
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.fruit_item, parent, false);
-        ViewHolder holder = new ViewHolder(view);
+        View view = LayoutInflater.from(parent.getContext()).inflate(mItemLayout, parent, false);
+        final ViewHolder holder = new ViewHolder(view);
+        holder.fruitView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int position = holder.getAdapterPosition();
+                Fruit fruit = mFruitList.get(position);
+                Toast.makeText(v.getContext(), "you clicked view " + fruit.getName(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        holder.fruitImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int position = holder.getAdapterPosition();
+                Fruit fruit = mFruitList.get(position);
+                Toast.makeText(v.getContext(), "you clicked image " + fruit.getName(), Toast.LENGTH_SHORT).show();
+            }
+        });
         return holder;
     }
 
--- RecyclerViewTest/app/src/main/java/azuredrop/recyclerviewtest/MainActivity.java
+++ RecyclerViewTest/app/src/main/java/azuredrop/recyclerviewtest/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.widget.LinearLayout;
 
 import java.util.ArrayList;
@@ -19,15 +20,32 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         initFruits();
-        RecyclerView recyclerView = (RecyclerView)findViewById(R.id.recycler_view);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
-        FruitAdapter adapter = new FruitAdapter(mFruitList);
-        recyclerView.setAdapter(adapter);
+
+        // vertical
+        RecyclerView recyclerView_vertical = (RecyclerView) findViewById(R.id.recycler_view_vertical);
+        LinearLayoutManager layoutManager_vertical = new LinearLayoutManager(this);
+        recyclerView_vertical.setLayoutManager(layoutManager_vertical);
+        FruitAdapter adapter_vertical = new FruitAdapter(mFruitList, R.layout.fruit_item_vertical);
+        recyclerView_vertical.setAdapter(adapter_vertical);
+
+        // vertical
+        RecyclerView recyclerView_horizontal = (RecyclerView) findViewById(R.id.recycler_view_horizontal);
+        LinearLayoutManager layoutManager_horizontal = new LinearLayoutManager(this);
+        layoutManager_horizontal.setOrientation(LinearLayoutManager.HORIZONTAL);
+        recyclerView_horizontal.setLayoutManager(layoutManager_horizontal);
+        FruitAdapter adapter_horizontal = new FruitAdapter(mFruitList, R.layout.fruit_item_horizontal);
+        recyclerView_horizontal.setAdapter(adapter_horizontal);
+
+        // stagger
+        RecyclerView recyclerView_stagger = (RecyclerView) findViewById(R.id.recycler_view_stagger);
+        StaggeredGridLayoutManager layoutManager_stagger = new StaggeredGridLayoutManager(4, StaggeredGridLayoutManager.VERTICAL);
+        recyclerView_stagger.setLayoutManager(layoutManager_stagger);
+        FruitAdapter adapter_stagger = new FruitAdapter(mFruitList, R.layout.fruit_item_stagger);
+        recyclerView_stagger.setAdapter(adapter_stagger);
     }
 
     private void initFruits(){
-        for (int i = 0; i < 2; i++) {
+        for (int i = 0; i < 3; i++) {
             Fruit apple = new Fruit("Apple", R.drawable.apple_pic);
             mFruitList.add(apple);
             Fruit banana = new Fruit("Banana", R.drawable.banana_pic);
