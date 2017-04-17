559aa640feb67c4f8d0bd36c9d9501582c66a4be
AzureDrop <taozhanbiao@gmail.com>
2017-04-17 14:33:56 +0800

    20170417 Chapter4  --  fragment

---
+++ FragmentTest/app/src/main/java/azuredrop/fragmenttest/AnotherRightFragment.java
@@ -0,0 +1,21 @@
+package azuredrop.fragmenttest;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by AzureDrop on 2017/4/17.
+ */
+
+public class AnotherRightFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.another_right_fragment, container, false);
+        return view;
+    }
+}
