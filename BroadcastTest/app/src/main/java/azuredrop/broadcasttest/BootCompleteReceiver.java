ef811dad2bc6bd20b58b7e3b9a365cf1869260c9
AzureDrop <taozhanbiao@gmail.com>
2017-04-18 14:13:22 +0800

    20170418_04 Chapter5  --  broadcast

---
+++ BroadcastTest/app/src/main/java/azuredrop/broadcasttest/BootCompleteReceiver.java
@@ -0,0 +1,14 @@
+package azuredrop.broadcasttest;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class BootCompleteReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "Boot Complete", Toast.LENGTH_LONG).show();
+    }
+}
--- BroadcastTest/app/src/main/java/azuredrop/broadcasttest/MainActivity.java
+++ BroadcastTest/app/src/main/java/azuredrop/broadcasttest/MainActivity.java
@@ -6,13 +6,20 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private IntentFilter intentFilter;
+
     private NetworkChangeReceiver networkChangeReceiver;
+    private LocalReceiver localReceiver;
+
+    private LocalBroadcastManager localBroadcastManager;
 
     class NetworkChangeReceiver extends BroadcastReceiver {
         @Override
@@ -31,20 +38,46 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    class LocalReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Toast.makeText(context, "received local broadcast", Toast.LENGTH_SHORT).show();
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // local broadcast
+        localBroadcastManager = LocalBroadcastManager.getInstance(this);
+
+        // send button
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent intent = new Intent("azuredrop.broadcasttest.MY_BROADCAST");
+                //sendOrderedBroadcast(intent, null);
+                Intent intent = new Intent("azuredrop.broadcasttest.LOCAL_BROADCAST");
+                localBroadcastManager.sendBroadcast(intent);
+            }
+        });
+
         intentFilter = new IntentFilter();
-        intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
-        networkChangeReceiver = new NetworkChangeReceiver();
-        registerReceiver(networkChangeReceiver, intentFilter);
+//        intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
+//        networkChangeReceiver = new NetworkChangeReceiver();
+//        registerReceiver(networkChangeReceiver, intentFilter);
+        intentFilter.addAction("azuredrop.broadcasttest.LOCAL_BROADCAST");
+        localReceiver = new LocalReceiver();
+        localBroadcastManager.registerReceiver(localReceiver, intentFilter);
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        unregisterReceiver(networkChangeReceiver);
+//        unregisterReceiver(networkChangeReceiver);
+        localBroadcastManager.unregisterReceiver(localReceiver);
     }
 }
