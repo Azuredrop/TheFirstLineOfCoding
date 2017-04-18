fc22a8e3fcce8df80960494cee10d10bc80e93e9
AzureDrop <taozhanbiao@gmail.com>
2017-04-18 10:50:59 +0800

    20170418_02 Chapter4  --  Best practice for fragment

---
+++ FragmentBestPractice/app/src/main/java/com/azuredrop/android/fragmentbestpractice/News.java
@@ -0,0 +1,24 @@
+package com.azuredrop.android.fragmentbestpractice;
+
+/**
+ * Created by Administrator on 2017/4/18.
+ */
+
+public class News {
+    private String title;
+    private String content;
+
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
---
+++ FragmentBestPractice/app/src/main/java/com/azuredrop/android/fragmentbestpractice/NewsContentFragment.java
@@ -0,0 +1,33 @@
+package com.azuredrop.android.fragmentbestpractice;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by Administrator on 2017/4/18.
+ */
+
+public class NewsContentFragment extends Fragment {
+    private View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.news_content_frag, container, false);
+        return view;
+    }
+
+    public void refresh(String newsTitle, String newsContent){
+        View visibilityLayout = view.findViewById(R.id.visibility_layout);
+        visibilityLayout.setVisibility(View.VISIBLE);
+        TextView newsTitleText = (TextView)view.findViewById(R.id.news_title);
+        newsTitleText.setText(newsTitle);
+        TextView newsContentText = (TextView)view.findViewById(R.id.news_content);
+        newsContentText.setText(newsContent);
+    }
+}
