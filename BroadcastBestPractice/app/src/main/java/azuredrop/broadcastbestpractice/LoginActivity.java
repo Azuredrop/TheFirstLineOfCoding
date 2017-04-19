f93cc7d70d92e298ec58cc9008411308241972ba
AzureDrop <taozhanbiao@gmail.com>
2017-04-19 15:52:52 +0800

    20170419_03 Chapter6 -- update of SharedPreferences

--- BroadcastBestPractice/app/src/main/java/azuredrop/broadcastbestpractice/LoginActivity.java
+++ BroadcastBestPractice/app/src/main/java/azuredrop/broadcastbestpractice/LoginActivity.java
@@ -1,16 +1,23 @@
 package azuredrop.broadcastbestpractice;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class LoginActivity extends BaseActivity {
+    private SharedPreferences preferences;
+    private SharedPreferences.Editor editor;
+
     private EditText accountEdit;
     private EditText passwordEdit;
+    private CheckBox rememberPass;
     private Button loginButton;
 
     @Override
@@ -18,10 +25,14 @@ public class LoginActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        // SharedPreferences
+        preferences = PreferenceManager.getDefaultSharedPreferences(this);
         // acount
         accountEdit = (EditText) findViewById(R.id.account);
         // password
         passwordEdit = (EditText) findViewById(R.id.password);
+        // remember pass
+        rememberPass = (CheckBox) findViewById(R.id.remember_pass);
         // login
         loginButton = (Button) findViewById(R.id.login);
         loginButton.setOnClickListener(new View.OnClickListener() {
@@ -32,6 +43,17 @@ public class LoginActivity extends BaseActivity {
 
                 // if account is 'admin' and password is '123456', then pass
                 if (account.equals("admin") && password.equals("123456")) {
+                    editor = preferences.edit();
+                    if (rememberPass.isChecked()) {
+                        editor.putString("account", account);
+                        editor.putString("password", password);
+                        editor.putBoolean("remember_password", true);
+                    } else {
+                        editor.clear();
+                    }
+                    editor.apply();
+
+                    // 启动活动
                     Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                     startActivity(intent);
                     finish();
@@ -40,5 +62,15 @@ public class LoginActivity extends BaseActivity {
                 }
             }
         });
+
+        // init
+        boolean isRemember = preferences.getBoolean("remember_password", false);
+        if (isRemember) {
+            String account = preferences.getString("account", "");
+            String password = preferences.getString("password", "");
+            accountEdit.setText(account);
+            passwordEdit.setText(password);
+            rememberPass.setChecked(true);
+        }
     }
 }
