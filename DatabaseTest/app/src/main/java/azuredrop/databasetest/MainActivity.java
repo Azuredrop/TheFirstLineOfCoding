82f649588d90a13a17ab33fcedd12a5990aa925a
AzureDrop <taozhanbiao@gmail.com>
2017-04-19 17:04:23 +0800

    20170419_04 Chapter6  --  storage by SQLite_part

---
+++ DatabaseTest/app/src/main/java/azuredrop/databasetest/MainActivity.java
@@ -0,0 +1,26 @@
+package azuredrop.databasetest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private MyDatabaseHelper dbHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        dbHelper = new MyDatabaseHelper(this, "BookStore.db", null, 2);
+        // create btn
+        Button createDatabase = (Button)findViewById(R.id.create_database);
+        createDatabase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dbHelper.getWritableDatabase();
+            }
+        });
+    }
+}
