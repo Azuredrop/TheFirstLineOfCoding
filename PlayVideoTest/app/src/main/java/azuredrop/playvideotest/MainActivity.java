d35ceecdc7fd31924dc7bc2981c2dcca4fc88fae
AzureDrop <taozhanbiao@gmail.com>
2017-04-28 17:17:30 +0800

    20170428_02 Chapter8  --  VideoView

---
+++ PlayVideoTest/app/src/main/java/azuredrop/playvideotest/MainActivity.java
@@ -0,0 +1,98 @@
+package azuredrop.playvideotest;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.widget.VideoView;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private VideoView videoView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Video
+        videoView = (VideoView) findViewById(R.id.video_view);
+        // play
+        Button play = (Button) findViewById(R.id.play);
+        play.setOnClickListener(this);
+        // pause
+        Button pause = (Button) findViewById(R.id.pause);
+        pause.setOnClickListener(this);
+        // replay
+        Button replay = (Button) findViewById(R.id.replay);
+        replay.setOnClickListener(this);
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+        } else {
+            initVideoPath();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (videoView != null) {
+            videoView.suspend();
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.play:
+                if (!videoView.isPlaying()) {
+                    videoView.start();
+                }
+                break;
+            case R.id.pause:
+                if (videoView.isPlaying()) {
+                    videoView.pause();
+                }
+                break;
+            case R.id.replay:
+                if (videoView.isPlaying()) {
+                    videoView.resume();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 1:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    initVideoPath();
+                } else {
+                    Toast.makeText(this, "拒绝权限将无法使用程序", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void initVideoPath() {
+        File file = new File(Environment.getExternalStorageDirectory(), "move.mp4");
+        videoView.setVideoPath(file.getPath());
+    }
+}
